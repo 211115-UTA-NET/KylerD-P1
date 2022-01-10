641483ffd765af0e80de450fd0e172cb08472130
Kyler Dennis <KylerGDennis@gmail.com>
2022-01-10 14:32:40 -0500

    Redo API program file

--- SpiceItUpAPI/SpiceItUp.Api/Program.cs
+++ SpiceItUpAPI/SpiceItUp.Api/Program.cs
@@ -7,9 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("SpiceItUp-DB-Connection");
 // Add services to the container.
 
-//bool prettyPrintJson = builder.Configuration.GetValue<string>("PrettyPrintJsonOutput") == "true";
+bool prettyPrintJson = builder.Configuration.GetValue<string>("PrettyPrintJsonOutput") == "true";
 
-builder.Services.AddControllers();/* (options =>
+builder.Services.AddControllers(options =>
 {
     options.InputFormatters.Add(new XmlSerializerInputFormatter(options));
     options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
@@ -17,7 +17,7 @@ builder.Services.AddControllers();/* (options =>
     var jsonFormatter = options.OutputFormatters.OfType<SystemTextJsonOutputFormatter>().First();
     jsonFormatter.SerializerOptions.WriteIndented = prettyPrintJson;
 });
-*/
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
